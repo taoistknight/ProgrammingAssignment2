8527020f88c15ec8d7868253a231e5addcfd4684
taoistknight <vincentccng@gmail.com>
2014-05-24 20:42:45 -0600

    Cleaning up whitespace
    Adjusted tabs for comments to show up neater in GitHub interface

--- cachematrix.R
+++ cachematrix.R
@@ -4,29 +4,30 @@
 ## - get a cached inverse of the stored matrix, and
 ## - solve and cache the inverse of stored matrix
 makeCacheMatrix <- function(x = matrix()) {
-	s <- NULL									#creating variable to store cached matrix solution
-	set <- function(y) {						# overwrites current stored matrix and clears cached solution
+	s <- NULL					#creating variable to store cached matrix solution
+	set <- function(y) {				# overwrites current stored matrix and clears cached solution
 		x <<- y
 		s <<- NULL
 	}
-	get <- function() x							#retrieves currently stored matrix
+	get <- function() x				#retrieves currently stored matrix
 	setinverse <- function(solve) s <<- solve	#calls solve function to generate inverse of stored matrix
-	getinverse <- function() s					#retrieves cached solution
+	getinverse <- function() s			#retrieves cached solution
 	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
 
-## Checks if a solution to the input matrix exists.  If it does, it returns the cached values, and otherwise solves the matrix.
+## Checks if a solution to the input matrix exists.  If it does, it returns the cached values
+## Otherwise, runs solve function to generate inverse matrix.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 		s <- x$getinverse()
-		if(!is.null(s)) {						#cached solution exists, print message and return cached value
+		if(!is.null(s)) {			#cached solution exists, print message...
 			message("getting cached data")
-			return(s)							#break out of function
+			return(s)			#return cached value and breaks out of function call
 		}
-		data<- x$get()							#retrive stored matrix value, and...
-		s <- solve(data, ...)					#plug into the solve() function to generate inverse matrix
-		x$setinverse(s)							#cache the generated solution
+		data<- x$get()				#retrive stored matrix value, and...
+		s <- solve(data, ...)			#plug into the solve() function to generate inverse matrix
+		x$setinverse(s)				#cache the generated solution
 		s
 }
